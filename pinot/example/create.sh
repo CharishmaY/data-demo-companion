ed6eabe32f8f466ad90c38e2228718dac36f7bea
CharishmaY <64750668+CharishmaY@users.noreply.github.com>
2024-04-27 16:12:08 -0500

    Update create.sh

--- pinot/example/create.sh
+++ pinot/example/create.sh
@@ -6,5 +6,11 @@ CONTENT_TYPE_HDR='Content-Type:application/json'
 URL=http://localhost:9000
 
 # create schema
-curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./schema.json" "$URL/schemas"
-curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./table.json" "$URL/tables"
+#curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./schema.json" "$URL/schemas"
+#curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./table.json" "$URL/tables"
+
+curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./venue.json" "$URL/schemas"
+curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./venueTable.json" "$URL/tables"
+
+curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./address.json" "$URL/schemas"
+curl -s -X POST -H $CONTENT_TYPE_HDR -d "@./addressLoader.json" "$URL/tables"
